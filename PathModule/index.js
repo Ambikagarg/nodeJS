1c36c7e8f652ad2ef6d1ec4c17c46914c0d88165
Ambikagarg <64024928+Ambikagarg@users.noreply.github.com>
2023-06-28 12:25:11 +0530

    Delete PathModule directory

--- PathModule/index.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-console.log(path.dirname("E:/nodejsYoutube/PathModule/index.js"));
-console.log(path.extname("E:/nodejsYoutube/PathModule/index.js"));
-console.log(path.basename("E:/nodejsYoutube/PathModule/index.js"));
-const mypath = path.parse("E:/nodejsYoutube/PathModule/index.js");
-console.log(mypath.name);
